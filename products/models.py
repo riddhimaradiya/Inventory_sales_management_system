38e6a153befd2a07ecb67542ff26f17af9e65f46
Riddhi Mardiya <riddhimardiya@gmail.com>
2026-08-17 17:02:57 +0530

    add product and stock movement models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    sku = models.CharField(max_length=50,unique=True, db_index=True)
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(0)])
+    gst_percentage = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    quantity = models.PositiveIntegerField(default=0)
+    threshold_quantity = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.name} ({self.sku})"
+
+
+class StockMovement(models.Model):
+    class MovementType(models.TextChoices):
+        PURCHASE = "PURCHASE", "Purchase"
+        SALE = "SALE", "Sale"
+        RESTOCK = "RESTOCK", "Restock"
+        RETURN = "RETURN", "Return"
+        ADJUSTMENT = "ADJUSTMENT", "Adjustment"
+
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="stock_movements")
+    Movement_Type = models.CharField(max_length=20,choices=MovementType.choices)
+    quantity = models.PositiveIntegerField()
+    reference = models.CharField(max_length=100, blank=True)
+    note = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(
+                fields=["product", "created_at"]
+            ),
+        ]
+
+    def __str__(self):
+        return (
+            f"{self.product.sku} - "
+            f"{self.Movement_Type} - " 
+            f"{self.quantity}"
+        )
